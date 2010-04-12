b4a3bf400b259f5501362642da44aeb12f3fb909
Mat Schaffer <mat.schaffer@gmail.com>
2010-04-12 08:45:34 -0400

    Removed *.bat before installing package per adamv's request.

--- Library/Formula/elasticsearch.rb
+++ Library/Formula/elasticsearch.rb
@@ -7,7 +7,7 @@ class Elasticsearch < Formula
   md5 'c438c0d18bef8d2efd2d801fa872a3fa'
 
   def install
+    FileUtils.rm_f Dir["bin/*.bat"]
     prefix.install %w[bin config lib]
-    FileUtils.rm_f Dir["#{bin}/*.bat"]
   end
 end
